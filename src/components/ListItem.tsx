d7c76bca09e12f9ed59583f38062711382ccc9b2
YEZI <yezi.dev@gmail.com>
2023-07-27 22:09:00 +0900

    List UI 작업

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,3 +1,16 @@
 export default function ListItem() {
-  return <div>ListItem</div>;
+  // 게시글 id, 제목, 내용, 회원id 생성일 수정일
+  return (
+    <li>
+      <div className="py-5 flex justify-between items-end gap-10">
+        <div className="cursor-pointer">
+          <h3 className="text-3xl">제목</h3>
+          <p className="mt-3 text-xl line-clamp-2">
+            내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+          </p>
+        </div>
+        <div className="shrink-0 text-lg text-gray-500">생성일</div>
+      </div>
+    </li>
+  );
 }
