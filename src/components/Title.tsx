2a3259d135428a88217daaa2ea390b090dfceb14
YEZI <yezi.dev@gmail.com>
2023-09-12 21:59:42 +0900

    댓글 ui

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,11 +2,21 @@ interface Props {
   title?: string;
   subtitle?: string;
   description?: string;
+  posttitle?: boolean;
 }
 
-export default function Title({ title, subtitle, description }: Props) {
+export default function Title({
+  title,
+  subtitle,
+  description,
+  posttitle,
+}: Props) {
   return (
-    <div className="pb-8 pt-6 border-b border-gray-200 space-y-5">
+    <div
+      className={`space-y-5  ${
+        posttitle ? '' : 'pb-8 pt-6 border-gray-200 border-b'
+      }`}
+    >
       {title && (
         <h2 className="text-3xl font-extrabold sm:text-4xl md:text-6xl">
           {title}
