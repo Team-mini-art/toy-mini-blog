4dc85fab6a4100efe4dbdde1d0ec30b98b7a3943
YEZI <yezi.dev@gmail.com>
2023-09-28 23:04:37 +0900

    comment API

---
+++ src/api/comment.ts
@@ -0,0 +1,46 @@
+import axiosCustom from './index';
+
+export const getPosts = async () => {
+  try {
+    const { data } = await axiosCustom.get('/api/posts');
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const postView = async (createForm: Record<string, string>) => {
+  try {
+    const { data } = await axiosCustom.post('/api/posts', createForm);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getView = async (id: string) => {
+  try {
+    const { data } = await axiosCustom.get(`/api/posts/${id}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const putView = async (id: string, putForm: Record<string, string>) => {
+  try {
+    const { data } = await axiosCustom.put(`/api/posts/${id}`, putForm);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteView = async (id: string) => {
+  try {
+    const { data } = await axiosCustom.delete(`/api/posts/${id}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
